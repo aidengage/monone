bb3635a9be33278f106c7f2de1bd140965df888d
aidengage <aidenfgage@gmail.com>
2025-12-07 21:04:12 -0500

    hard coded marker for churchill downs

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,23 @@
 import React from 'react';
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import MapView, {Marker, PROVIDER_GOOGLE } from 'react-native-maps';
 import { StyleSheet, View } from 'react-native';
 
 export default function App() {
     return (
         <View style={styles.container}>
             <MapView style={styles.map} /*provider={PROVIDER_GOOGLE}*/
-            initialRegion={{
-                latitude: 38.2037,
-                longitude: -85.7724,
-                latitudeDelta: 0.09,
-                longitudeDelta: 0.03,
-            }}/>
+                initialRegion={{
+                    latitude: 38.2037, //N
+                    longitude: -85.7724, //W
+                    latitudeDelta: 0.09,
+                    longitudeDelta: 0.03,
+                }}
+            >
+                <Marker
+                    coordinate={{latitude: 38.2037, longitude: -85.7724}}
+                    title={"churchill downs"}
+                />
+            </MapView>
         </View>
     );
 }
