36190d45c0f36f338739d074d4698b774242e744
aidengage <aidenfgage@gmail.com>
2025-12-19 04:00:31 -0500

    normalized corner radius across android and ios

--- app/index.tsx
+++ app/index.tsx
@@ -4,53 +4,44 @@ import { StyleSheet, View } from 'react-native';
 
 export default function App() {
     return (
-        <View style={styles.container}>
-            <MapView style={styles.map} /*provider={PROVIDER_GOOGLE}*/
-                initialRegion={{
-                    latitude: 38.2037, //N
-                    longitude: -85.7724, //W
-                    latitudeDelta: 0.09,
-                    longitudeDelta: 0.03,
-                }}
-            >
-                <Marker
-                    coordinate={{latitude: 38.2037, longitude: -85.7724}}
-                    title={"Churchill Downs"}
-                />
-            </MapView>
+        <View style={styles.mask}>
+            <View style={styles.container}>
+                <MapView renderToHardwareTextureAndroid={true} style={styles.map} /*provider={PROVIDER_GOOGLE}*/
+                         initialRegion={{
+                             latitude: 38.2037, //N
+                             longitude: -85.7724, //W
+                             latitudeDelta: 0.09,
+                             longitudeDelta: 0.03,
+                         }}
+                >
+                    <Marker
+                        coordinate={{latitude: 38.2037, longitude: -85.7724}}
+                        title={"Churchill Downs"}
+                    />
+                </MapView>
+            </View>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
+    mask: {
+        width: '100%',
+        height: '100%',
         overflow: 'hidden',
-        flexDirection: 'column',
         padding: 20,
+    },
+    container: {
+        overflow: 'hidden',
+
+        borderRadius: 45,
+        borderColor: '#ed7070',
     },
     map: {
         width: '100%',
         height: '100%',
-        borderRadius: 90,
-        borderWidth: 1,
+
+        borderRadius: 45,
+        borderColor: '#4169ff',
     },
 });
-
-
-
-// import { Text, View } from "react-native";
-//
-// export default function Index() {
-//   return (
-//     <View
-//       style={{
-//         flex: 1,
-//         justifyContent: "center",
-//         alignItems: "center",
-//       }}
-//     >
-//       <Text>Edit app/index.tsx to edit this screen.</Text>
-//     </View>
-//   );
-// }
