59c1a8473f1a40062cf20418a6809421eae30797
aidengage <aidenfgage@gmail.com>
2025-12-21 16:07:58 -0500

    added marker + changed tints

--- spots/ContentView.swift
+++ spots/ContentView.swift
@@ -11,9 +11,10 @@ import MapKit
 struct ContentView: View {
     var body: some View {
         Map {
-            Marker("Churchill Downs", coordinate: CLLocationCoordinate2D(latitude: 38.20306, longitude: -85.77000))
-            Marker("Muhammad Ali Airport", coordinate: CLLocationCoordinate2D(latitude: 38.17410, longitude: -85.73650))
-            Marker("Cherokee Park", coordinate:CLLocationCoordinate2D(latitude: 38.24120, longitude: -85.69690))
+            Marker("Churchill Downs", coordinate: CLLocationCoordinate2D(latitude: 38.20306, longitude: -85.77000)).tint(.orange)
+            Marker("Muhammad Ali Airport", coordinate: CLLocationCoordinate2D(latitude: 38.17410, longitude: -85.73650)).tint(.blue)
+            Marker("Cherokee Park", coordinate:CLLocationCoordinate2D(latitude: 38.24120, longitude: -85.69690)).tint(.green)
+            Marker("Falls of the Ohio", coordinate:CLLocationCoordinate2D(latitude:38.2731227, longitude: -85.7591351)).tint(.green)
         }.cornerRadius(60).padding(15)
     }
 }
