7cefee8154d163c5cd27ab01c812e15d1aa5f17d
aidengage <aidenfgage@gmail.com>
2025-12-21 15:59:21 -0500

    markers added to map + round corners

--- spots/ContentView.swift
+++ spots/ContentView.swift
@@ -6,19 +6,15 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
     var body: some View {
-//        Map {
-//            Marker("Churchill Downs", CoordinateSpace.global(latitude: 38.20306, longitude: -85.77000))
-//        }
-//        VStack {
-//            Image(systemName: "globe")
-//                .imageScale(.large)
-//                .foregroundStyle(.tint)
-//            Text("Hello, world!")
-//        }
-//        .padding()
+        Map {
+            Marker("Churchill Downs", coordinate: CLLocationCoordinate2D(latitude: 38.20306, longitude: -85.77000))
+            Marker("Muhammad Ali Airport", coordinate: CLLocationCoordinate2D(latitude: 38.17410, longitude: -85.73650))
+            Marker("Cherokee Park", coordinate:CLLocationCoordinate2D(latitude: 38.24120, longitude: -85.69690))
+        }.cornerRadius(60).padding(15)
     }
 }
 
