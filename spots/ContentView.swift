b14db26972bf26e9224fc2145a252f7e4cb4eaec
aidengage <aidenfgage@gmail.com>
2025-12-21 17:28:52 -0500

    marker color edited

--- spots/ContentView.swift
+++ spots/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
         Map {
             Marker("Churchill Downs", coordinate: CLLocationCoordinate2D(latitude: 38.20306, longitude: -85.77000)).tint(.orange)
             Marker("Muhammad Ali Airport", coordinate: CLLocationCoordinate2D(latitude: 38.17410, longitude: -85.73650)).tint(.blue)
-            Marker("Cherokee Park", coordinate:CLLocationCoordinate2D(latitude: 38.24120, longitude: -85.69690)).tint(.green)
+            Marker("Cherokee Park", coordinate:CLLocationCoordinate2D(latitude: 38.24120, longitude: -85.69690)).tint(.gray)
             Marker("Falls of the Ohio", coordinate:CLLocationCoordinate2D(latitude:38.2731227, longitude: -85.7591351)).tint(.green)
         }.cornerRadius(60).padding(15)
     }
