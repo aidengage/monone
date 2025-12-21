88ed7e3636d5335f9b5297b0cc00f6aa99e18467
aidengage <aidenfgage@gmail.com>
2025-12-21 15:12:33 -0500

    created xcode swift project :)

---
+++ spots/spotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  spotsApp.swift
+//  spots
+//
+//  Created by Aiden Gage on 12/21/25.
+//
+
+import SwiftUI
+
+@main
+struct spotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
