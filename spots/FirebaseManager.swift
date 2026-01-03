95566e4acd41d91bfc7907f50862d0a7ded218a6
aidengage <aidenfgage@gmail.com>
2026-01-03 00:31:00 -0500

    deleted old functions that didnt work

--- spots/FirebaseManager.swift
+++ spots/FirebaseManager.swift
@@ -23,56 +23,6 @@ class FirebaseManager {
         self.storage = Storage.storage()
     }
     
-    func getPostLocation(postCoords: (Double, Double)) {
-        fs.collection("post").document(String(postCoords.0)).collection("post").document(String(postCoords.1)).getDocument { (document, error) in
-            if let document = document, document.exists {
-                print("Document data: \(document.data() ?? [:])")
-            } else {
-                print("Document does not exist")
-            }
-        }
-    }
-    
-    func getPostByName(postName: String) {
-        fs.collection("post").document(postName).getDocument { (document, error) in
-            if let document = document, document.exists {
-                print("Document data: \(document.data() ?? [:])")
-            } else {
-                print("Document does not exist")
-            }
-        }
-    }
-    
-    func getPostById(postID: String) {
-        fs.collection("post").document(postID).getDocument { (document, error) in
-            if let document = document, document.exists {
-                print("document does exist")
-            } else {
-                print("Document does not exist")
-            }
-        }
-    }
-    
-    func addSnapListener() {
-        fs.collection("post").addSnapshotListener { (snapshot, error) in
-            if let error = error {
-                print("Error connecting: ", error)
-            } else {
-                print("connected to firestore")
-            }
-        }
-    }
-    
-    func getPostBy_u9mohOI4BugTXOh0tYgQ() {
-        fs.collection("post").document("u9mohOI4BugTXOh0tYgQ").getDocument { (document, error) in
-            if let document = document, document.exists {
-                print("document u9mohOI4BugTXOh0tYgQ does exist")
-            } else {
-                print("Document u9mohOI4BugTXOh0tYgQ does not exist")
-            }
-        }
-    }
-    
     func getDocs() {
         fs.collection("post").getDocuments { (querySnapshot, error) in
             if let error = error {
