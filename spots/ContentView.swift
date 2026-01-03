243e34dcaed9d81f5e6360167d2ce00fd28d58be
aidengage <aidenfgage@gmail.com>
2026-01-03 17:50:06 -0500

    liquid glass set, looks more frosted

--- spots/ContentView.swift
+++ spots/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
     var body: some View {
         ZStack(alignment: .bottom) {
             Map {
-                Marker("Churchill Downs", coordinate: CLLocationCoordinate2D(latitude: 38.20306, longitude: -85.77000)).tint(.orange).annotationSubtitles(.visible)
+                Marker("Churchill Downs", coordinate: CLLocationCoordinate2D(latitude: 38.20306, longitude: -85.77000)).tint(.orange)
                 Marker("Muhammad Ali Airport", coordinate: CLLocationCoordinate2D(latitude: 38.17410, longitude: -85.73650)).tint(.blue)
                 Marker("Cherokee Park", coordinate:CLLocationCoordinate2D(latitude: 38.24120, longitude: -85.69690)).tint(.gray)
                 Marker("Falls of the Ohio", coordinate:CLLocationCoordinate2D(latitude:38.2731227, longitude: -85.7591351)).tint(.green)
@@ -22,27 +22,25 @@ struct ContentView: View {
             .padding(10)
             .ignoresSafeArea(.all)
             
-            Button(action: getPostById) {
-//                Text("more actions")
-//                    .font(Font.title)
+            Button(action: {}) {
 
                 Menu {
-                    Button {
-                    } label: {
-                        Label("do something here", systemImage: "heart")
+                    Button(action: getDocs) {
+                        Label("Print Docs", systemImage: "document")
                     }
-                    Button {
-                    } label: {
+                    Button(action: {}) {
                         Label("add post", systemImage: "mappin")
                     }
                 } label: {
                     Text("more actions")
                         .font(Font.title)
-                        .padding(-8)
+//                        .padding(-8)
                 }
             }
-            .buttonStyle(.borderedProminent)
-            .shadow(color: .blue, radius: 10, y: 5)
+            .buttonStyle(.glass)
+//            .tint(Color(.clear))
+            .glassEffect()
+//            .shadow(color: .clear, radius: 10, y: 5)
 //            .contextMenu {
 //                Button {
 //                } label: {
@@ -59,7 +57,7 @@ struct ContentView: View {
 
 let fm = FirebaseManager()
 
-func getPostById() {
+func getDocs() {
 //    fm.getPostBy_u9mohOI4BugTXOh0tYgQ()
     fm.getDocs()
 }
