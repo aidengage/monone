2b6533168f71516d51b0296979b3211378c475d2
aidengage <aidenfgage@gmail.com>
2026-01-04 22:37:38 -0500

    ability to add posts to database

--- spots/AddPostView.swift
+++ spots/AddPostView.swift
@@ -8,25 +8,50 @@
 import SwiftUI
 
 struct AddPostView: View {
+    @State var title: String = ""
+    @State var description: String = ""
+    @State var address: String = ""
+    @State var imageURL: String = ""
+    @State var latitude: Double = 0.0
+    @State var longitude: Double = 0.0
+    @State var rating: Double = 0.0
+    
     var body: some View {
         VStack {
 
             Form {
                 Section(header: Text("Add a new post")) {
-                    TextField("Title", text: .constant(""))
-                    TextField("Description", text: .constant(""))
-                    TextField("Address", text: .constant(""))
+                    TextField("Title", text: $title)
+                    TextField("Description", text: $description)
+                    TextField("Address", text: $address)
+                    TextField("Rating", value: $rating, format: .number)
+                        .keyboardType(.decimalPad)
                 }
                 Section(header: Text("Image URL")) {
-                    TextField("image upload wip", text: .constant(""))
+                    TextField("image upload wip", text: $imageURL)
                 }
                 Section(header: Text("Coordinates")) {
                     HStack {
-                        TextField("Latitude", text: .constant(""))
-                        TextField("Longitude", text: .constant(""))
+                        
+                        TextField("Latitude", value: $latitude, format: .number)
+                            .keyboardType(.decimalPad)
+                        TextField("Longitude", value: $longitude, format: .number)
+                            .keyboardType(.decimalPad)
                     }
                 }
             }
+            let fm = FirebaseManager()
+            Button(action: {
+                if imageURL.isEmpty || title.isEmpty || address.isEmpty || description.isEmpty || String(rating).isEmpty || String(latitude).isEmpty || String(longitude).isEmpty {
+                    print("add every value to post")
+                } else {
+                    fm.addPost(image: imageURL, name: title, address: address, rating: rating, description: description, coords: (latitude, longitude))
+                }
+            }) {
+                Label("Post!", systemImage: "plus")
+            }
+            .buttonStyle(.glassProminent)
+            .glassEffect()
         }
         .navigationTitle("Add Post")
     }
