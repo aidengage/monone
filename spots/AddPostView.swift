8e40e79ac72b58aee36d801cfd301560a4c7d9a3
aidengage <aidenfgage@gmail.com>
2026-01-04 19:48:46 -0500

    setup vstack and preview

--- spots/AddPostView.swift
+++ spots/AddPostView.swift
@@ -5,3 +5,16 @@
 //  Created by Aiden Gage on 1/4/26.
 //
 
+import SwiftUI
+
+struct AddPostView: View {
+    var body: some View {
+        VStack {
+            
+        }
+    }
+}
+
+#Preview {
+    AddPostView()
+}
