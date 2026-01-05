3e86bb1c2bbde04cdf174ff4bf186c8e81cd3742
aidengage <aidenfgage@gmail.com>
2026-01-04 19:54:55 -0500

    created add post form front end

--- spots/AddPostView.swift
+++ spots/AddPostView.swift
@@ -10,7 +10,22 @@ import SwiftUI
 struct AddPostView: View {
     var body: some View {
         VStack {
-            
+            Form {
+                Section(header: Text("Add a new post")) {
+                    TextField("Title", text: .constant(""))
+                    TextField("Description", text: .constant(""))
+                    TextField("Address", text: .constant(""))
+                }
+                Section(header: Text("Image URL")) {
+                    TextField("image upload wip", text: .constant(""))
+                }
+                Section(header: Text("Coordinates")) {
+                    HStack {
+                        TextField("Latitude", text: .constant(""))
+                        TextField("Longitude", text: .constant(""))
+                    }
+                }
+            }
         }
     }
 }
