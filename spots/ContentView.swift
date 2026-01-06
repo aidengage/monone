79286e5aa676bf2c017a922ad0cb18633f630348
mkhan2025 <107653894+mkhan2025@users.noreply.github.com>
2026-01-06 15:43:37 -0500

    Update ContentView.swift

--- spots/ContentView.swift
+++ spots/ContentView.swift
@@ -105,7 +105,7 @@ struct ContentView: View {
                     } else {
                         //change button visibility to false
                         
-                        Text("Not logged in")
+                        
                             
                     }
                 }
