043294cd3ed4165b595409a25e8d643ec759122f
aidengage <aidenfgage@gmail.com>
2026-01-29 16:15:12 -0500

    creating db migration class

---
+++ spots/Services/Firebase/DBMigration.swift
@@ -0,0 +1,14 @@
+//
+//  DBMigration.swift
+//  spots
+//
+//  Created by Aiden Gage on 1/29/26.
+//
+
+import SwiftUI
+
+class DBMigration {
+    func migrateDatabase() async {
+        print("migrating db from subcollections to top level")
+    }
+}
