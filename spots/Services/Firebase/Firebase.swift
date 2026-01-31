fb1d741e2da12daee6f65a2ee2cfdd910b40a3d8
aidengage <aidenfgage@gmail.com>
2026-01-30 23:24:42 -0500

    renamed post, rating, user files and deleted auth, store, and migration files

--- spots/Services/Firebase/Firebase.swift
+++ spots/Services/Firebase/Firebase.swift
@@ -26,71 +26,12 @@ final class Firebase {
         return auth
     }
     
-    // function proxies to separate out firebase implementation
-    // storage
     func getStorage() -> Storage {
         return storage.getStorage()
     }
     
-    // user
-//    func getCurrentUserID() -> String {
-//        return auth.getCurrentUserID()
-//    }
     
-    // user
-//    func getCurrentUser() -> FirebaseAuth.User? {
-//        return auth.getCurrentUser()
-//    }
-    
-    // user
-//    func logout() {
-//        auth.logout()
-//    }
-    // post
-//    func getAllPosts(completion: @escaping ([PostMan]) -> Void) {
-//        store.getAllPosts(completion: completion)
-//    }
-    
-    // post (dont really need this, can query easily
-//    func getUserPosts(completion: @escaping ([PostMan]) -> Void) {
-//        return store.getUserPosts(completion: completion)
-//    }
-    
-    //post
-//    func addPost(images: [String], name: String, address: String, rating: Decimal, ratingCount: Int, comment: String, coords: (lat: Double, long: Double), selectedActivity: String) async {
-//        await store.addPost(images: images, name: name, address: address, rating: rating, ratingCount: ratingCount, comment: comment, coords: (lat: coords.lat, long: coords.long), selectedActivity: selectedActivity)
-//    }
-    
-    // post (dont need anymore)
-//    func addPostIDToUser(postID: String) {
-//        store.addPostIDToUser(postID: postID)
-//    }
-    
-    // rating
-//    func getPostRatings(postOwner: String, postId: String, completion: @escaping ([Rating]) -> Void) {
-//        store.getPostRatings(postOwner: postOwner, postId: postId, completion: completion)
-//    }
-    
-    // rating
-//    func getPostAverageRatings(postId: String) async throws -> Decimal {
-//        return try await store.getPostAverageRatings(postId: postId)
-//    }
-    
-    // user
-//    func addUser(uid: String, email: String, username: String, posts: [String]) {
-//        store.addUser(uid: uid, email: email, username: username)
-//    }
-    
-    // rating (dont need anymore dont think)
-//    func addPostToRated(postID: String) {
-//        store.addPostToRated(postID: postID)
-//    }
-    
-    // rating
-//    func addRatingToPost(postOwner: String, postId: String, userId: String, rating: Decimal, comment: String) async {
-//        await store.addRatingToPost(postOwner: postOwner, postId: postId, userId: userId, rating: rating, comment: comment)
-//    }
-    
+        
     // storage
     func uploadImage(uuidArray: [String], data: [Data]) async throws {
         try await storage.uploadImage(uuidArray: uuidArray, data: data)
