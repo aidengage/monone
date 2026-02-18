41e466e4cf43fda80963561fff2e00323c23b18a
aidengage <aidenfgage@gmail.com>
2026-02-17 23:14:02 -0500

    unit testing files created

---
+++ spotsTests/spotsTests.swift
@@ -0,0 +1,18 @@
+//
+//  spotsTests.swift
+//  spotsTests
+//
+//  Created by Aiden Gage on 2/17/26.
+//
+
+import Testing
+
+@testable import spots
+
+struct spotsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
