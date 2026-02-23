99a66aadc66cc563e77872ab9516c6169265ee32
aidengage <aidenfgage@gmail.com>
2026-02-22 22:45:45 -0500

    started following this tutorial, might just push when im finished with tutorial
    https://www.youtube.com/watch?v=ik1QRc_kN9M

--- spots/Features/Camera/CameraView.swift
+++ spots/Features/Camera/CameraView.swift
@@ -1,2 +1,29 @@
 
 import SwiftUI
+import AVFoundation
+import Combine
+
+class CameraManager: NSObject, ObservableObject, AVCapturePhotoCaptureDelegate, AVCaptureFileOutputRecordingDelegate {
+    
+    func fileOutput(_ output: AVCaptureFileOutput, didFinishRecordingTo outputFileURL: URL, from connections: [AVCaptureConnection], error: (any Error)?) {
+        <#code#>
+    }
+    
+    
+    @Published var capturedImage: UIImage?
+    @Published var isSessionRunning = false
+    @Published var authStatus: AVAuthorizationStatus = .notDetermined
+    @Published var isRecording = false
+    
+    let session = AVCaptureSession()
+    
+    private let photoOutput = AVCapturePhotoOutput()
+    private let videoOutput = AVCaptureMovieFileOutput()
+    private var currentInput: AVCaptureDeviceInput?
+    
+    private let sessionQueue = DispatchQueue(label: "com.monone.camera.sessionQueue")
+    
+    override init() {
+        super.init()
+    }
+}
