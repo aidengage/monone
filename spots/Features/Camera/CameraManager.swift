60c991eca21e63dd253ad9fbd3630ad33eb42298
aidengage <aidenfgage@gmail.com>
2026-02-23 00:06:01 -0500

    capture photo and save photo

--- spots/Features/Camera/CameraManager.swift
+++ spots/Features/Camera/CameraManager.swift
@@ -1,2 +1,131 @@
 
 import SwiftUI
+import AVFoundation
+import Combine
+
+class CameraManager: NSObject, ObservableObject, AVCapturePhotoCaptureDelegate {
+    
+//    func fileOutput(_ output: AVCaptureFileOutput, didFinishRecordingTo outputFileURL: URL, from connections: [AVCaptureConnection], error: (any Error)?) {
+//        <#code#>
+//    }
+    
+    
+    @Published var capturedImage: IdentifiableImage?
+    @Published var isSessionRunning = false
+    @Published var authorizationStatus: AVAuthorizationStatus = .notDetermined
+    @Published var isRecording = false
+    
+    let session = AVCaptureSession()
+    
+    private let photoOutput = AVCapturePhotoOutput()
+    private let videoOutput = AVCaptureMovieFileOutput()
+    private var currentInput: AVCaptureDeviceInput?
+    
+    private let sessionQueue = DispatchQueue(label: "com.monone.camera.sessionQueue")
+    
+    override init() {
+        super.init()
+    }
+    
+    func checkAuth() {
+        switch AVCaptureDevice.authorizationStatus(for: .video) {
+            
+        case .authorized:
+            authorizationStatus = .authorized
+            setupSession()
+        case .notDetermined:
+            authorizationStatus = .notDetermined
+            AVCaptureDevice.requestAccess(for: .video) { [weak self] granted in
+                DispatchQueue.main.async {
+                    self?.authorizationStatus = granted ? .authorized : .denied
+                    if granted {
+                        self?.setupSession()
+                    }
+                }
+            }
+        case .denied:
+            authorizationStatus = .denied
+        @unknown default:
+            authorizationStatus = .denied
+        
+        }
+    }
+    
+    private func setupSession() {
+        sessionQueue.async {
+            [weak self] in
+            guard let self = self else { return }
+            
+            //quality
+            self.session.beginConfiguration()
+            self.session.sessionPreset = .photo
+            
+            //cam input
+            guard let camera = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back), let input = try? AVCaptureDeviceInput(device: camera) else {
+                print("failed to access camera")
+                self.session.commitConfiguration()
+                return
+            }
+            
+            if self.session.canAddInput(input) {
+                self.session.addInput(input)
+                self.currentInput = input
+            }
+            
+            // add output
+            if self.session.canAddOutput(self.photoOutput) {
+                self.session.addOutput(self.photoOutput)
+                
+                self.photoOutput.isHighResolutionCaptureEnabled = true
+//                self.photoOutput.maxPhotoDimensions = .init(width: 1920, height: 1080)
+                self.photoOutput.maxPhotoQualityPrioritization = .quality
+            }
+            
+            self.session.commitConfiguration()
+            
+            //start
+            self.session.startRunning()
+            
+            DispatchQueue.main.async {
+                self.isSessionRunning = self.session.isRunning
+            }
+        }
+    }
+    
+    func capturePhoto() {
+        sessionQueue.async {
+            [weak self] in
+            guard let self = self else { return }
+            
+            let settings = AVCapturePhotoSettings()
+            settings.flashMode = .off
+            
+            if self.photoOutput.isHighResolutionCaptureEnabled {
+                settings.isHighResolutionPhotoEnabled = true
+            }
+            self.photoOutput.capturePhoto(with: settings, delegate: self)
+        }
+    }
+    
+    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+        if let error = error {
+            print("photo capture error: \(error.localizedDescription)")
+            return
+        }
+        
+        guard let imageData = photo.fileDataRepresentation(), let uiImage = UIImage(data: imageData) else {
+            print("failed to convert photo to image")
+            return
+        }
+        
+        DispatchQueue.main.async {
+            [weak self] in
+            self?.capturedImage = IdentifiableImage(image: uiImage)
+        }
+    }
+}
+
+struct IdentifiableImage: Identifiable {
+    let id = UUID()
+    let image: UIImage
+}
--- spots/Features/User/SignupView.swift
+++ spots/Features/User/SignupView.swift
@@ -53,35 +53,62 @@ struct SignupView: View {
                     
 //                    .fullScreenCover(isPresented: $showCamera) {
                     .sheet(isPresented: $showCamera) {
-                        if cameraManager.authorizationStatus == .authorized {
-                            CameraPreview(session: cameraManager.session)
-                                .ignoresSafeArea()
-                        } else {
-                            Image(systemName: "camera.fill.badge.xmark")
-                                .font(.largeTitle)
-                                .foregroundStyle(Color(.systemGray2))
-                            Text("Camera Permission Required")
-                                .font(.largeTitle)
-                                .foregroundStyle(Color(.systemGray2))
-                            
-                            if cameraManager.authorizationStatus == .denied {
-                                Text("please enable camera in settings")
+                        ZStack {
+                            if cameraManager.authorizationStatus == .authorized {
+                                CameraPreview(session: cameraManager.session)
+                                    .ignoresSafeArea()
+                            } else {
+                                Image(systemName: "camera.fill.badge.xmark")
+                                    .font(.largeTitle)
+                                    .foregroundStyle(Color(.systemGray2))
+                                Text("Camera Permission Required")
+                                    .font(.largeTitle)
+                                    .foregroundStyle(Color(.systemGray2))
                                 
-                                Button("open settings?") {
-                                    if let settingsURL = URL(string: UIApplication.openSettingsURLString) {
-                                        UIApplication.shared
-                                            .open(settingsURL)
+                                if cameraManager.authorizationStatus == .denied {
+                                    Text("please enable camera in settings")
+                                        .font(.subheadline)
+                                        .foregroundStyle(.secondary)
+                                        .multilineTextAlignment(.center)
+                                        .padding(.horizontal)
+                                    
+                                    Button("open settings?") {
+                                        if let settingsURL = URL(string: UIApplication.openSettingsURLString) {
+                                            UIApplication.shared
+                                                .open(settingsURL)
+                                        }
                                     }
+                                    .buttonStyle(.glassProminent)
                                 }
-                                .buttonStyle(.glassProminent)
-                                .tint(Color.indigo)
+                            }
+                            VStack {
+                                Spacer()
+                                
+                                Button {
+                                    cameraManager.capturePhoto()
+                                } label: {
+                                    Circle()
+                                        .strokeBorder(.white, lineWidth: 3)
+                                        .frame(width: 70, height: 70)
+                                        .overlay {
+                                            Circle()
+                                                .fill(.white)
+                                                .frame(width: 60, height: 60)
+                                        }
+                                }
+                                .padding(.bottom, 20)
+                            }
+                            .sheet(item: $cameraManager.capturedImage) { item in
+                                PhotoPreviewView(item: item, onDismiss: {
+                                    cameraManager.capturedImage = nil
+                                })
                             }
                         }
+                        .onAppear {
+                            cameraManager.checkAuth()
+                        }
+                        .padding()
                     }
-                    .onAppear {
-                        cameraManager.checkAuth()
-                    }
-                    .padding()
                 }
                 
                 Button(action: {
