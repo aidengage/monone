a919c294612e7ea5e159b09a5a1883cb29f3a2a5
aidengage <aidenfgage@gmail.com>
2026-06-09 19:18:56 -0600

    started auth service file
    added google sign in
    imported apple auth
    added nncredentialkit to try and make managing sign in a bit easier

---
+++ spots/Services/AuthenticationService.swift
@@ -0,0 +1,13 @@
+//
+//  AuthenticationService.swift
+//  spots
+//
+//  Created by Aiden Gage on 6/9/26.
+//
+//  https://firebase.google.com/docs/auth/ios/google-signin
+//  https://github.com/nikolainobadi/NnCredentialKit
+
+import AuthenticationServices
+import CryptoKit
+import GoogleSignIn
+import FirebaseAuth
