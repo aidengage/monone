96ec6b92e5e805a66f25e7e768c76b38b0044896
aidengage <aidenfgage@gmail.com>
2026-06-23 19:49:55 -0600

    working on bit of a restructure

---
+++ spots/Features/Post/PostInteractionViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  PostInteractionViewModel.swift
+//  spots
+//
+//  Created by Aiden Gage on 6/22/26.
+//
+
+import SwiftUI
+
+@Observable class PostInteractionViewModel {
+    var bookmarks: [String] = []
+    var following: [String] = []
+}
---
+++ spots/Services/UserService.swift
@@ -0,0 +1,12 @@
+//
+//  UserService.swift
+//  spots
+//
+//  Created by Aiden Gage on 6/22/26.
+//
+
+import SwiftUI
+
+@Observable class UserService {
+    
+}
